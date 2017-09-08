ee2e2faad30a55ab743434f34f283d9f943ae809
Jan von der Assen <jan.vda@gmx.ch>
2017-09-08 14:53:12 +0200

    Add model and methods for user management.

--- routes/login.js
+++ routes/login.js
@@ -6,21 +6,67 @@ var mongoose = require('mongoose');
 var favicon = require('favicon');
 var async = require('async');
 var mainapp = require('../app');
+var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
+
+// mongodburl = 'mongodb://' + 'localhost' + ':' + '27017' + '/bookmarks';
+//
+// mongoose.connect(mongodburl);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+var userSchema = mongoose.Schema({
+  password: String
+, userName: String
+, name: String
+});
+
+userSchema.methods.verifyPassword = function(pw) {
+  if (this.password === pw) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+var User = mongoose.model('User', userSchema);
+
+User.find(function(err, bookmarks){
+  if (err) return console.error(err);
+});
+
+// var admin = new User({password: 'gg', userName: 'jan', name: 'jann'});
+//
+// admin.save(function(err, admin){
+//   if (err) {
+//     return console.err(err)
+//   }
+//   console.log('saved: ', admin)
+// });
+
 passport.use(new LocalStrategy(
   function(username, password, done) {
-      if (username == 'jan' && password == 'gg') {
-          return done(null, {name: '', id: '123'})
-      }
-      else if (username == 'dd' && password == 'mmmo') {
-          return done(null, {name: '', id: '345'})
-
-      }
-      else {
-          return done(null, false, {message: 'Incorrect credentials.'})
-      }
+      // if (username == 'jan' && password == 'gg') {
+      //     return done(null, {name: '', id: '123'})
+      // }
+      // else if (username == 'dd' && password == 'mmmo') {
+      //     return done(null, {name: '', id: '345'})
+      //
+      // }
+      // else {
+      //     return done(null, false, {message: 'Incorrect credentials.'})
+      // }s
+    User.findOne({ userName: username }, function (err, user) {
+      console.log('authenticating: ', user)
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
   }
 ));
 
@@ -29,16 +75,13 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(id, done) {
-    if (id === '123') {
-        return done(null, {name: 'jan', id: '123'});
-    }
-    else if (id === '345') {
-         return done(null, {name: 'mart', id: '345'})
-    }
-    else {
-        console.error('Error deserialicing user.')
-    }
+  if (id.match(/^[0-9a-fA-F]{24}$/)) {
+  // Yes, it's a valid ObjectId, proceed with `findById` call.
 
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  }
 });
 
 /* GET users listing. */
